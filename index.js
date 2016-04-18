3e0eeda0189e0c5b8b60f14251c19c035879565f
nulltask <nulltask@gmail.com>
2016-04-18 16:10:57 +0900

    display logs on watching

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 const http = require('http');
 const express = require('express');
+const logger = require('morgan');
 
 const app = express();
 const server = http.createServer(app);
 
 app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
 app.use(express.static(`${__dirname}/.tmp`));
 app.use(express.static(`${__dirname}/public`));
 
