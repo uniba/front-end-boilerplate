a79e17bfa2f94ae6214e4c80e928413937765d25
nulltask <nulltask@gmail.com>
2017-03-02 17:12:47 +0900

    :lipstick:

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,8 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader',
-          'postcss-loader']
+          'postcss-loader'
+        ]
       },
       {
         test: /\/javascripts\/.*\.css$/,
