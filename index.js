c6d41424cec09d2d2ef9e1de0a2a24a003fc57a5
nulltask <nulltask@gmail.com>
2017-03-02 17:15:46 +0900

    mount webpack-dev-middleware

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 const http = require('http');
 const express = require('express');
 const logger = require('morgan');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackConfig = require('./webpack.config');
 
 const app = express();
 const server = http.createServer(app);
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
-app.use(express.static(`${__dirname}/.tmp`));
+app.use(webpackMiddleware(webpack(webpackConfig)));
 app.use(express.static(`${__dirname}/public`));
 
 server.listen(app.get('port'), () => {
